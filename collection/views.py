3e3ae15365622baad4fb49c56d077f57796c6e84
Ricky Burgess <ricky.burgess@xodusgroup.com>
2025-06-01 22:15:48 +0100

    Refactor gallery view to improve search and filtering functionality

--- collection/views.py
+++ collection/views.py
@@ -42,7 +42,7 @@ def gallery(request):
     all_tags = Tag.objects.all()
     
     # Pagination
-    paginator = Paginator(images, 25)
+    paginator = Paginator(images, 12)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
